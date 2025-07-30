6dc7706da7dfe56f873cd8b128f8366865e5e1f0
nithya2374 <nithyasri17012004@gmail.com>
2025-07-30 08:40:41 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -35,7 +35,13 @@ io.on("connection", (socket) => {
 });
 
 app.use(cors({ 
-    origin: "http://localhost:5173", credentials: true
+    origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }, credentials: true
  }));
  
 app.use(express.json());
