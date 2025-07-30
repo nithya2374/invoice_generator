3bc1550cdf1dc4dda99c9583be3d5caf8a88050f
nithya2374 <nithyasri17012004@gmail.com>
2025-07-30 07:17:32 +0530

    add component

--- client/src/Layout/AdminLayout.jsx
+++ client/src/Layout/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect} from "react";
-import AdminSidebar from "../components/AdminSidebar";
-import AdminHeader from "../components/AdminHeader";
+import AdminSidebar from "../components/adminsidebar";
+import AdminHeader from "../components/adminheader";
 import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
